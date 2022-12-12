32ada220c3c41f51fc96f2f517134d62ce31afe3
stevencarlislewalker <stevencarlislewalker@gmail.com>
2022-12-11 22:41:02 -0500

    function to apply the same method over multiple objects

---
+++ R/method_apply.R
@@ -0,0 +1,15 @@
+#' Method Apply
+#'
+#' Call a method for each item in a list of objects.
+#'
+#' @param objects List of objects.
+#' @param method_name Character string giving the name of the method.
+#' @param ... Arguments to pass to the method.
+#' @export
+method_apply = function(objects, method_name, ...) {
+  output_objects = list()
+  for (i in seq_along(objects)) {
+    output_objects[[i]] = objects[[i]][[method_name]](...)
+  }
+  setNames(output_objects, names(objects))
+}
